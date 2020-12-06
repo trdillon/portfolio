2bea5c75c483d8e0004c3e1fec4782fee7c61b65
Tim Dillon <tim@itsdits.com>
2020-12-06 14:50:15 +0900

    Added font-display: swap to fonts.css to speed up font display on page load

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './service-worker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
