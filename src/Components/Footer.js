b145fe3655e3476483139e56bd081ce5deb2edf7
Tim Dillon <tim@itsdits.com>
2020-12-07 08:58:46 +0900

    Added aria-labels to links without text or images

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,8 @@ class Footer extends Component {
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}/></a></li>
+        return <li key={network.name}><a href={network.url} aria-label={network.name}>
+            <i className={network.className}/></a></li>
       })
     }
 
@@ -22,7 +23,8 @@ class Footer extends Component {
                       <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
                   </ul>
               </div>
-              <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"/></a></div>
+              <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home">
+                  <i className="icon-up-open"/></a></div>
           </div>
       </footer>
     );
